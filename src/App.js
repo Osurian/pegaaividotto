ccf3c92027bf14d844aea9cc2c46ac695d0920f2
Matheus Osorio <rodriguesm@alunos.utfpr.edu.br>
2022-08-04 20:27:33 -0300

    tarefa1

---
+++ src/App.js
@@ -0,0 +1,442 @@
+import './App.css';
+import React from "react";
+
+function App() {
+  return (
+        <>
+        <div className="cardapio-entrada">
+          <h1 id="topico">Entradas</h1>
+        </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> E01-Croquete de Bacalhau  </h2>
+          <p className="card-description">(10 unidades) serve 2P</p>
+          <h2 className="valor">R$: 40,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> E02-Burrata </h2>
+          <p className="card-description">(queijo de búfala, torradas e tomates confitados) Serve 2P</p>
+          <h2 className="valor">R$: 52,00 </h2>
+        </div>
+      </div>
+      
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> E03-Cesto de Salada de Parmesão com Mix de Folhas</h2>
+          <p className="card-description">Serve 2p</p>
+          <h2 className="valor">R$: 32,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> E04-Salada Italiana com cubos de frango</h2>
+          <p className="card-description">Serve 2p</p>
+          <h2 className="valor">R$: 28,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> E05-Tabua Adega</h2>
+          <p className="card-description">(queijo, azeitona, salame e torradinhas) Serve 2p</p>
+          <h2 className="valor">R$: 45,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> E06-Tabua Quatro Queijos Adega</h2>
+          <p className="card-description">(provolone,nozinho, parmesão e gouda) Serve 2p</p>
+          <h2 className="valor">R$: 55,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> E07-Tábua Provolera</h2>
+          <p className="card-description">(provolone na pedra com orégano) Serve 2p</p>
+          <h2 className="valor">R$: 32,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> E08-Salada Caprese</h2>
+          <p className="card-description">(tomates cereja ao azeite,manjericão e queijo bolinha) Serve 2p</p>
+          <h2 className="valor">R$: 30,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> E09-Trio de Bruschettas</h2>
+          <p className="card-description">(alho poró – cogumelos – tomates cereja) Serve 2p</p>
+          <h2 className="valor">R$: 35,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> E10-Brie com Mel</h2>
+          <p className="card-description">(queijo brie, mel, castanhas e torradinhas) Serve 2p</p>
+          <h2 className="valor">R$: 58,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> E11-Chapa Adega</h2>
+          <p className="card-description">(picanha grelhada, tomate cereja e queijo provolone) Serve 2p</p>
+          <h2 className="valor">R$: 110,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> E12-Ostras Gratinadas</h2>
+          <p className="card-description">(08 unidades) Serve 2p</p>
+          <h2 className="valor">R$: 66,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> E13-Bolinho de Carne seca </h2>
+          <p className="card-description">(08 unidades) Serve 2p</p>
+          <h2 className="valor">R$: 32,00 </h2>
+        </div>
+      </div>
+      
+
+      <div className="cardapio-entrada">
+        <h1 id="topico">Pratos</h1>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> P01-Prime Rib com Risoto de Brócolis e Tomates </h2>
+          <p className="card-description">Serve 2p</p>
+          <h2 className="valor">R$: 115,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> P02-Fettuccine com Tomates, Manjericão, Mignon Grelhado ao Molho Gorgonzola </h2>
+          <p className="card-description">Serve 2p</p>
+          <h2 className="valor">R$: 110,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> P03-Lasanha de Carne Desfiada </h2>
+          <p className="card-description">Serve 2p</p>
+          <h2 className="valor">R$: 69,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> P04-Lasanha Quatro Queijos </h2>
+          <p className="card-description"> Serve 2p</p>
+          <h2 className="valor">R$: 69,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> P05-Spaghetti Aglio e Oli, com Beff Ancho grelhado </h2>
+          <p className="card-description">Serve 1p</p>
+          <h2 className="valor">R$: 65,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> P06-Fettuccine Al Mare </h2>
+          <p className="card-description">(lula, camarão, mariscos e azeite provençal) Serve 1p</p>
+          <h2 className="valor">R$: 90,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> P07-Fettuccine com Nata e Salmão </h2>
+          <p className="card-description">Serve 1p</p>
+          <h2 className="valor">R$: 70,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> P08-Ossobuco com Polenta cremosa </h2>
+          <p className="card-description">(08 unidades) Serve 1p</p>
+          <h2 className="valor">R$: 65,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> P09-Risotto al Porro e Mignon na mostarda dijon </h2>
+          <p className="card-description">Serve 1p</p>
+          <h2 className="valor">R$: 69,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> P10-Risotto Zucchini e Parmesão  </h2>
+          <p className="card-description">(abobrinha) Serve 1p</p>
+          <h2 className="valor">R$: 59,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> P11-Risotto de Maça Verde com Carré de Cordeiro grelhado </h2>
+          <p className="card-description">Serve 1p</p>
+          <h2 className="valor">R$: 32,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> E13-Bolinho de Carne seca </h2>
+          <p className="card-description">(08 unidades) Serve 2p</p>
+          <h2 className="valor">R$: 32,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> P12-Risotto al Funghi com Cubos de Mignon </h2>
+          <p className="card-description">Serve 1p</p>
+          <h2 className="valor">R$: 69,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> P13-Risotto de Gorgonzola e Entrecôte grelhado </h2>
+          <p className="card-description">Serve 1p</p>
+          <h2 className="valor">R$: 69,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> P14-Risotto alla Milanesa com agu de cordeiro </h2>
+          <p className="card-description">Serve 1p</p>
+          <h2 className="valor">R$: 70,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> P15-Risotto Mare e Monti  </h2>
+          <p className="card-description">(bacon, camarão, champignon e vinho branco) Serve 2p</p>
+          <h2 className="valor">R$: 75,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> E16-Risotto al Limão Siciliano c/Salmão Grellhado </h2>
+          <p className="card-description">Serve 1p</p>
+          <h2 className="valor">R$: 75,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> P17- Ravióli de Ricota e Cogumelo  </h2>
+          <p className="card-description">(Vinho branco e bacon)Serve 1p</p>
+          <h2 className="valor">R$: 70,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> P18-Prato Kids  </h2>
+          <p className="card-description">(Penne, mignon em cubos e fritas) Serve 1p</p>
+          <h2 className="valor">R$: 45,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> P19-Adicional de Beef Ancho </h2>
+          <p className="card-description">1 unidade</p>
+          <h2 className="valor">R$: 20,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> P20-Adicional de Medalhão de Mignon </h2>
+          <p className="card-description">1 uniadde</p>
+          <h2 className="valor">R$: 20,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> P21-Adicional de Posta Salmão</h2>
+          <p className="card-description">1 uniadde</p>
+          <h2 className="valor">R$: 20,00 </h2>
+        </div>
+      </div>
+
+
+      <div className="cardapio-entrada">
+        <h1 id="topico">Calzones</h1>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> C01-Calzone Parisiense </h2>
+          <p className="card-description">(Molho de tomate, presunto, frango desfiado, requeijão, ervilha e mussarela) Serve 2p</p>
+          <h2 className="valor">R$: 65,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'> C02-Calzone do Chefe </h2>
+          <p className="card-description">(Molho de tomate, calabresa, azeitonas, palmito e mussarela) Serve 2p</p>
+          <h2 className="valor">R$: 65,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'>C03-Strogonoff de Mignon  </h2>
+          <p className="card-description">(Molho de tomate, Mignon em tiras, mussarela e parmesão) Serve 2p</p>
+          <h2 className="valor">R$: 70,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'>C04-Calzone de Calabresa  </h2>
+          <p className="card-description"> (Molho de tomate, calabresa, bacon e mussarela) Serve 2p</p>
+          <h2 className="valor">R$: 65,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'>C05-Calzone a Jardineira </h2>
+          <p className="card-description">(Molho de tomate, palmito, brócolis, ervilha, tomates em cubo, catupiry e mussarela) Serve 2p</p>
+          <h2 className="valor">R$: 65,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'>C06-Calzone Prestígio </h2>
+          <p className="card-description">(Leite condensado, chocolate e coco ralado e mussarela) Serve 2p</p>
+          <h2 className="valor">R$: 65,00 </h2>
+        </div>
+      </div>
+
+
+      <div className="cardapio-entrada">
+        <h1 id="topico">Pizzas</h1>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'>Z01-Zucchini  </h2>
+          <p className="card-description">(Muçarela, molho, abobrinha e queijo parmesão) Serve 2p</p>
+          <h2 className="valor">R$: 65,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'>Z02-Caprese </h2>
+          <p className="card-description">(Muçarela, molho, Tomate,Pesto de azeitona preta, queijo de búfala) Serve 2p</p>
+          <h2 className="valor">R$: 65,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'>Z03-Margherita</h2>
+          <p className="card-description"> (Muçarela, molho, tomate, manjericão)  Serve 2p</p>
+          <h2 className="valor">R$: 65,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'>Z04-Parma e Rúcula </h2>
+          <p className="card-description">(Muçarela, molho, tomate seco, presunto e rúcula) Serve 2p</p>
+          <h2 className="valor">R$: 65,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'>Z05-Salame Milano </h2>
+          <p className="card-description"> (Muçarela, molho e salame milano) Serve 2p</p>
+          <h2 className="valor">R$: 65,00 </h2>
+        </div>
+      </div>
+
+
+      <div className="cardapio-entrada">
+        <h1 id="topico">Sobremesas</h1>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'>S01-Grand Gateau </h2>
+          <p className="card-description">(Morangos, sorvete chocolate Branco ou Preto e castanhas) Serve 1p</p>
+          <h2 className="valor">R$: 32,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'>S02-Panna Cotta  </h2>
+          <p className="card-description">(Leite cozido com calda de frutas vermelhas) Serve 2p</p>
+          <h2 className="valor">R$: 23,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'>S03-Verrine de Gianduia  </h2>
+          <p className="card-description"> (Duas camadas de chocolate com nutella e avelã) Serve 1p</p>
+          <h2 className="valor">R$: 32,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'>S04-Apfelstrudel </h2>
+          <p className="card-description">(torta de maça e sorvete de creme) Serve 2p</p>
+          <h2 className="valor">R$: 26,00 </h2>
+        </div>
+      </div>
+
+      <div className="cardapio">
+        <div id="item-cardapio">
+          <h2 className='titulo'>S05-Diamante Chocolate  </h2>
+          <p className="card-description">(Chocolate recheado com cream cheese, castanhas e doce de leite) Serve 1p</p>
+          <h2 className="valor">R$: 32,00 </h2>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
